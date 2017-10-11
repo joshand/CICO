d03ebc859463fc51e69bbbbeafc7562a2cd8300e
Josh Anderson <joshanderson@josh-anderson-mbp.local>
2017-10-10 22:49:13 -0700

    Update to enable Meraki dashboard link parser and Umbrella S3 log stats

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ import cico_meraki
 import cico_spark_call
 import cico_combined
 import cico_common
+import cico_umbrella
+import sys
 
 # Retrieve required details from environment variables
 bot_email = os.getenv("SPARK_BOT_EMAIL")
 spark_token = os.getenv("SPARK_BOT_TOKEN")
 bot_url = os.getenv("SPARK_BOT_URL")
 bot_app_name = os.getenv("SPARK_BOT_APP_NAME")
+
+if not bot_email or not spark_token or not bot_url or not bot_app_name:
+    print("Missing Environment Variable.")
+    sys.exit()
 
 # Create a new bot
 bot = SparkBot(bot_app_name, spark_bot_token=spark_token,
@@ -22,6 +28,9 @@ if cico_common.meraki_support():
 if cico_common.spark_call_support():
     bot.add_command('/spark-health', 'Get health of Spark environment.', cico_spark_call.get_spark_call_health_html)
     bot.add_command('/spark-check', 'Check Spark user status.', cico_spark_call.get_spark_call_clients_html)
+if cico_common.umbrella_support():
+    bot.add_command('/umbrella-health', 'Get health of Umbrella envrionment.', cico_umbrella.get_umbrella_health_html)
+    bot.add_command('/umbrella-check', 'Check Umbrella user status.', cico_umbrella.get_umbrella_clients_html)
 bot.add_command('/health', 'Get health of entire environment.', cico_combined.get_health)
 bot.add_command('/check', 'Get user status.', cico_combined.get_clients)
 
